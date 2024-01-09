d754189517f3c7d836a3c3bc0d6a8d5270f1f679
Hyuk Min <judicious210@gmail.com>
2024-01-09 20:21:42 +0900

    💯 Chal(Progs - BT): Solve 4 challenges (35/124)

---
+++ Challenges/Programmers/Basic Training/35. 배열 만들기 4.py
@@ -0,0 +1,8 @@
+
+
+# - pop
+# Takes index (when given, else take last), removes value at that index, and returns the value.
+# - remove
+# Takes value, removes first occurrence, and returns nothing.
+# - delete
+# Takes index, removes value at that index, and returns nothing.
